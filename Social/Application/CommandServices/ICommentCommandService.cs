ad6e7707d6ecf4c9774c170f029dcf341b7aa33e
Leonardo Rafael Garcia Villanueva <leonardo_351461@hotmail.com>
2026-06-28 19:27:09 -0500

    Update ICommentCommandService.cs

--- Social/Application/CommandServices/ICommentCommandService.cs
+++ Social/Application/CommandServices/ICommentCommandService.cs
@@ -6,5 +6,6 @@ namespace MonitoNet.Backend.Social.Application.CommandServices;
 public interface ICommentCommandService
 {
     Task<PublicationComment?> AddAsync(string publicationId, CreateCommentCommand command);
+    Task<PublicationComment?> UpdateAsync(string publicationId, string commentId, UpdateCommentCommand command);
     Task<bool> DeleteAsync(string publicationId, string commentId);
 }
